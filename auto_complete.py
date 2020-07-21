7b5a8b8bcb7f03e9eaee911476e4c80fd6e95c45
shreibers <ss@sem-wolf.org>
2020-07-21 23:10:36 +0300

    Functionality of inputs with incorrect / missing / unnecessary characters and support for reading from a file

--- auto_complete.py
+++ auto_complete.py
@@ -1,10 +1,14 @@
+the_file = open("copyright.txt")
 
-mockData = ["Hello nice World", "Hello World"]
+
+big_data = the_file.read().split("\n")
 data = {}
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+         's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 
 
 def init_data():
-    for index, sentence in enumerate(mockData):
+    for index, sentence in enumerate(big_data):
         length = len(sentence)
         for i in range(length):
             for j in range(i, length):
@@ -15,15 +19,58 @@ def init_data():
                     data[sentence[i:j + 1]].add(index)
 
 
+def delete(substring):
+    length = len(substring)
+
+    for i in range(length):
+        intersection_set = set()
+        if data.get(substring[:i+1]) and data.get(substring[i+2:length]):
+            intersection_set = data[substring[:i+1]].intersection(data[substring[i+2:length]])
+        for index_sub_str in intersection_set:
+            if substring[:i+1] + substring[i+2:length] in big_data[index_sub_str]:
+                return big_data[index_sub_str]
+
+
+def replace(substring):
+    length = len(substring)
+
+    for i in range(length):
+        intersection_set = set()
+        if data.get(substring[:i + 1]) and data.get(substring[i + 2:length]):
+            intersection_set = data[substring[:i + 1]].intersection(data[substring[i + 2:length]])
+        for index in intersection_set:
+            for char in chars:
+                if substring[:i + 1] + char + substring[i + 2:length] in big_data[index]:
+                    return big_data[index]
+
+
+def add_letter(substring):
+    length = len(substring)
+
+    for i in range(length):
+        intersection_set = set()
+        if data.get(substring[:i]) and data.get(substring[i:length]):
+            intersection_set = data[substring[:i]].intersection(data[substring[i:length]])
+        for index in intersection_set:
+            for char in chars:
+                if substring[:i] + char + substring[i:length] in big_data[index]:
+                    return big_data[index]
+
+
 def get_best_k_completions(substring):
+    new_substring = add_letter(substring)
+    if new_substring:
+        return new_substring
+
     if data.get(substring):
-        return mockData[list(data[substring])[0]]
+        return big_data[list(data[substring])[0]]
     return ""
 
 
 
+
 init_data()
-print(get_best_k_completions("lo W"))
+print(get_best_k_completions("nie"))
 
 
 
