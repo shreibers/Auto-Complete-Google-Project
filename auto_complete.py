fd25551664b0098577c53834769323ca0126a7c4
shreibers <ss@sem-wolf.org>
2020-07-23 03:20:01 +0300

    refactor the unexact strings input and divide into short functions

--- auto_complete.py
+++ auto_complete.py
@@ -1,23 +1,52 @@
 
 from auto_compliete_data import AutoCompleteData
 from init_data import Init
+import itertools
+import collections
+
+AMOUNT_COMPLETIONS = 5
+
+
+def data_from_file(file_name):
+    with open(file_name) as the_file:
+        sentences = the_file.readlines()
+    return [x.strip() for x in sentences]
+
 
 print("loading the files and preparing the system...")
-
-init = Init()
-init.init_data()
-
+list_data = data_from_file("copyright.txt")
+data = {}
 
 
-def get_score_of_replace_letter(input_string, after_changed):
-    num_for_reduce = 5
-    len_input = len(input_string)
-    for i in range(len_input):
-        if not input_string[i] == after_changed[i]:
-            return (len_input-1) * 2 - num_for_reduce
-        num_for_reduce = num_for_reduce - 1 if i < 4 else 2
+def init_data():
+    for index, sentence in enumerate(list_data):
+        length = len(sentence)
+        for i in range(length):
+            for j in range(i, length):
+                if not data.get(sentence[i:j + 1]):
+                    data[sentence[i:j + 1]] = set()
+                    data[sentence[i:j + 1]].add(index)
+                else:
+                    data[sentence[i:j + 1]].add(index)
 
-    return (len_input-1) * 2 - num_for_reduce
+
+
+# init = Init()
+# init.init_data()
+
+
+
+def replace_min_score(res, score, index):
+    min_score = min(res.values())
+    if min_score < score:
+        for key, value in res.items():
+            if value == min_score:
+                del res[key]
+                break
+
+        res[list_data[index]] = score
+    else:
+        res[list_data[index]] = score
 
 
 def with_delete(substring, num_to_search):
@@ -25,20 +54,16 @@ def with_delete(substring, num_to_search):
     len_ = len(substring)
 
     for i in range(len_):
-        index_substring_for_search = init.data.get(substring.replace(substring[i], "", 1))
+        index_substring_for_search = data.get(substring.replace(substring[i], "", 1))
+
         if index_substring_for_search:
             for index in index_substring_for_search:
                 score = (len_ - 1) * 2 - (10 - 2 * index) if i < 4 else (len_ - 1) * 2 - 2
-                res[init.list_data[index]] = score
-                if len(res.keys()) == num_to_search:
-                    min_score = min(res.values())
-                    if min_score < score:
-                        for key, value in res.items():
-                            if value == min_score:
-                                del res[key]
-                                break
+                res[list_data[index]] = score
 
-                    res[init.list_data[index]] = score
+                if len(res.keys()) == num_to_search:
+                    replace_min_score(res, score, index)
+
     return res
 
 
@@ -48,23 +73,17 @@ def with_replace(substring, num_to_search):
 
     for i in range(len_):
         for char in range(ord('a'), ord('z') + 1):
-            index_substring_for_search = init.data.get(substring.replace(substring[i], chr(char), 1))
+            index_substring_for_search = data.get(substring.replace(substring[i], chr(char), 1))
+
             if index_substring_for_search:
+
                 for index in index_substring_for_search:
                     score = (len_-1)*2 - (5 - index) if i < 4 else (len_-1)*2 - 1
-                    res[init.list_data[index]] = score
+                    res[list_data[index]] = score
+
                     if len(res.keys()) == num_to_search:
-                        min_score = min(res.values())
-                        if min_score < score:
-                            for key, value in res.items():
-                                if value == min_score:
-                                    del res[key]
-                                    break
+                        replace_min_score(res, score, index)
 
-                        res[init.list_data[index]] = score
-
-                    else:
-                        res[init.list_data[index]] = score
     return res
 
 
@@ -74,48 +93,65 @@ def with_add(substring, num_to_search):
 
     for i in range(len_+1):
         for char in range(ord('a'), ord('z') + 1):
-            index_substring_for_search = init.data.get(substring[:i] + chr(char) + substring[i:])
+            index_substring_for_search = data.get(substring[:i] + chr(char) + substring[i:])
+
             if index_substring_for_search:
                 for index in index_substring_for_search:
+
                     score = len_ * 2 - (10 - 2 * index) if i < 4 else (len_ - 1) * 2 - 2
-                    res[init.list_data[index]] = score
+                    res[list_data[index]] = score
+
                     if len(res.keys()) == num_to_search:
-                        min_score = min(res.values())
-                        if min_score < score:
-                            for key, value in res.items():
-                                if value == min_score:
-                                    del res[key]
-                                    break
-
-                        res[init.list_data[index]] = score
-
-                    else:
-                        res[init.list_data[index]] = score
+                        replace_min_score(res, score, index)
     return res
 
-def search(substring):
-    max_scores = {}
-    indexes = [5]
-    if init.data.get(substring):
-        indexes = list(init.data[substring]) if len(init.data[substring]) <= 5 else list(init.data[substring])[:5]
-    max_score = len(substring)
+
+def get_completions_without_changes(substring):
+    best_k_completions = {}
+    indexes = []
+    max_score = 0
+    if data.get(substring):
+        indexes = list(data[substring]) if len(data[substring]) <= AMOUNT_COMPLETIONS else \
+            list(data[substring])[:AutoCompleteData]
+        max_score = len(substring)*2
     len_ = len(indexes)
-    for i in range(len_):
-        max_scores[init.list_data[indexes[i]]] = max_score*2
+    for j in range(len_):
+        best_k_completions[list_data[indexes[j]]] = max_score
 
-    if len(max_scores) < 5:
-        max_scores = with_replace(substring, 5-len(max_scores))
-
-    return sorted(max_scores.keys())
+    return best_k_completions
 
 
-while 1:
-    input_ = input("The system is ready. Enter your text:\n")
-    if input_ == '#':
-        break
-    else:
+def get_completions_with_changes(substring, num_to_search):
 
-        print(search(input_))
+    res = with_replace(substring, num_to_search)
+    res.update(with_add(substring, num_to_search))
+    res.update(with_replace(substring, num_to_search))
+
+    return res
+
+    # return itertools.islice(collections.OrderedDict(res).items(), num_to_search)
+
+
+def get_best_k_completions(substring):
+    best_k_completions = get_completions_without_changes(substring)
+    len_ = len(best_k_completions.keys())
+
+    if len_ < AMOUNT_COMPLETIONS:
+        best_k_completions.update(get_completions_with_changes(substring, AMOUNT_COMPLETIONS - len_))
+
+    return sorted(best_k_completions.keys())
+
+init_data()
+print(get_best_k_completions(input()))
+# while 1:
+#     input_ = input("The system is ready. Enter your text:\n")
+#     while input_ != "":
+#         print(input_, end='')
+#         input_ += input()
+#         if input_ == '#':
+#               input_ = ""
+#         else:
+#           print(get_best_k_completions(input_))
 
 
 
--- init_data.py
+++ init_data.py
@@ -3,7 +3,6 @@ import glob
 
 class Init:
     def __init__(self):
-        self.sub_str_data = {}
         self.list_data = list()
         self.data = {}
 
@@ -13,19 +12,22 @@ class Init:
         return [x.strip() for x in sentences]
 
     def init_data(self):
-        txt_files = glob.glob("python-3.8.4-docs-text/python-3.8.4-docs-text/*.txt")
+        # txt_files = glob.glob("python-3.8.4-docs-text/python-3.8.4-docs-text/*.txt")
+        #
+        # for file in txt_files:
+        #     self.list_data += self.data_from_file(file)
+        # self.list_data += self.data_from_file("copyright.txt")
+        self.list_data = ["Hello world", "Hello Ruti", "Sari", "World kuyti", "Ruti&Sari" ]
+        for index, sentence in enumerate(self.list_data):
+            length = len(sentence)
+            for i in range(length):
+                for j in range(i, length):
+                    if not self.data.get(sentence[i:j + 1]):
+                        self.data[sentence[i:j + 1]] = set()
+                        self.data[sentence[i:j + 1]].add(index)
+                    else:
+                        self.data[sentence[i:j + 1]].add(index)
 
-        for file in txt_files:
-            self.list_data += self.data_from_file(file)
 
-            for index, sentence in enumerate(self.list_data):
-                length = len(sentence)
-                for i in range(length):
-                    for j in range(i, length):
-                        if not self.sub_str_data.get(sentence[i:j + 1]):
-                            self.data[sentence[i:j + 1]] = set()
-                            self.data[sentence[i:j + 1]].add(index)
-                        else:
-                            self.data[sentence[i:j + 1]].add(index)
 
 
