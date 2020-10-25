8ba7cb525721137bffcb74fe9bd8bb7e84077ae4
sari <ss@sem-wolf.ord>
2020-10-25 19:59:06 +0200

    offline: load data into pkl and json file

--- offline/init_data.py
+++ offline/init_data.py
@@ -1,4 +1,7 @@
+
 import glob
+import pickle as pkl
+import json
 
 from offline.auto_complete_data import AutoCompleteData
 k = 5
@@ -6,30 +9,39 @@ k = 5
 
 class Init:
     def __init__(self):
-        self.list_data = list()
-        self.data = {}
+        self.sentences = list()
+        self.sub_sentences = dict()
+
 
     def data_from_file(self, file_name):
         with open(file_name) as file_in:
             offset = 1
             for line in file_in:
-                self.list_data.append(AutoCompleteData(line.strip(), file_name, offset, 0))
+                self.sentences.append(AutoCompleteData(line.strip(), file_name, offset, 0))
                 offset += 1
 
     def init_data(self):
-        txt_files = glob.glob("../python-3.8.4-docs-text/python-3.8.4-docs-text/*.txt")
+        txt_files = glob.glob("../data/python-3.8.4-docs-text/python-3.8.4-docs-text/*.txt")
+
         for file in txt_files:
             self.data_from_file(file)
-        # data_from_file("copyright.txt")
-        for index, sentence in enumerate(self.list_data):
+
+        with open("../utils/data.pkl", 'wb') as file:
+            pkl.dump(self.sentences, file)
+
+        for index, sentence in enumerate(self.sentences):
             length = len(sentence.get_completed())
 
             for i in range(length):
                 for j in range(i, length):
                     sub_str = (sentence.get_completed()[i:j + 1]).lower()
-                    if not self.data.get(sub_str):
-                        self.data[sub_str] = set()
-                        self.data[sub_str].add(index)
+                    if not self.sub_sentences.get(sub_str):
+                        self.sub_sentences[sub_str] = list()
+                        self.sub_sentences[sub_str].append(index)
 
-                    elif len(self.data.get(sub_str)) < k:
-                        self.data[sub_str].add(index)
+                    elif len(self.sub_sentences.get(sub_str)) < k:
+                        self.sub_sentences[sub_str].append(index)
+                        self.sub_sentences[sub_str] = list(set(self.sub_sentences[sub_str]))
+
+        with open("../utils/sub_strings.json", 'w') as file:
+            json.dump(self.sub_sentences, file)
---
+++ offline/run_offline.py
@@ -0,0 +1,12 @@
+from offline.init_data import Init
+
+
+def main():
+    init = Init()
+
+    print("loading the files and preparing the system...")
+    init.init_data()
+
+
+if __name__ == '__main__':
+    main()
